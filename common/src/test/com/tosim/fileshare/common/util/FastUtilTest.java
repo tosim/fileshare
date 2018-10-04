eba48b5f15878c8b72da6a4a9848a39dada4dc1f
Sq-List <574146616@qq.com>
2018-10-04 22:02:35 +0800

    创建FastDFS操作类

---
+++ common/src/test/com/tosim/fileshare/common/util/FastUtilTest.java
@@ -0,0 +1,54 @@
+package com.tosim.fileshare.common.util;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class FastUtilTest {
+
+    @Test
+    public void upload() throws IOException {
+        File file = new File("D:\\nginx.conf");
+        byte[] content = new byte[(int)file.length()];
+        new FileInputStream(file).read(content);
+
+        System.out.println(FastDFSUtil.getInstance().upload(content, "conf"));
+    }
+
+    @Test
+    public void delete() {
+        System.out.println(FastDFSUtil.getInstance().delete("group1/M00/00/00/wKiJZ1u2HHWAAECHAAAT_QFZvSc38.conf"));
+    }
+
+    @Test
+    public void download() {
+        File file = new File("D:\\nginx-1.conf");
+        FileOutputStream fileOutputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+            bufferedOutputStream.write(FastDFSUtil.getInstance().download("group1/M00/00/00/wKiJZ1u2HHWAAECHAAAT_QFZvSc38.conf"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedOutputStream != null) {
+                try {
+                    bufferedOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
